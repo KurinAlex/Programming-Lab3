1686a88367a3683254c19b9fe3319ea7ae287d3a
Kurin, Alex <olexkurin@gmail.com>
2021-09-23 20:56:56 +0300

    lab3.2b 1.0))))

--- Programming-Lab3.2b/Programming-Lab3.2b.c
+++ Programming-Lab3.2b/Programming-Lab3.2b.c
@@ -4,7 +4,7 @@ int main()
 {
 	int n;
 	float x;
-	float P = 1;
+	float P = 1.0;
 
 	printf("Enter size of number sequence:\n");
 	scanf_s("%d", &n);
@@ -15,9 +15,9 @@ int main()
 		{
 			printf("Enter x%d:\n", i + 1);
 			scanf_s("%f", &x);
-			if (x != 1)
+			if (x != 1.0)
 			{
-				P *= x * x / (1 - x);
+				P *= x * x / (1.0 - x);
 				break;
 			}
 			printf("x can`t be equal to 1\n");
