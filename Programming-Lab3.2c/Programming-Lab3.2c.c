78f3faaf6c67a67b6583c8874ddd9495c3da6b95
Kurin, Alex <olexkurin@gmail.com>
2021-09-23 20:52:47 +0300

    lab3.2c

---
+++ Programming-Lab3.2c/Programming-Lab3.2c.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int n;
+	int m;
+	float P = 1.0;
+
+	printf("Enter n:\n");
+	scanf_s("%d", &n);
+
+	printf("Enter m:\n");
+	scanf_s("%d", &m);
+
+	for (int i = 0; i < n; i++)
+	{
+		P *= (float)(m - i) / (i + 1);
+	}
+
+	printf("P = %.0f", P);
+
+	getch();
+}
